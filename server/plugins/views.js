a329c8e5ecf82c3e082c7b28c1e0aaebb785c8f9
davidjamesstone <davidjamesstone@gmail.com>
2022-04-29 08:57:05 +0100

    Initial commit

---
+++ server/plugins/views.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const nunjucks = require('nunjucks')
+const config = require('../config')
+const pkg = require('../../package.json')
+const analyticsAccount = config.analyticsAccount
+
+module.exports = {
+  plugin: require('@hapi/vision'),
+  options: {
+    engines: {
+      html: {
+        compile: (src, options) => {
+          const template = nunjucks.compile(src, options.environment)
+
+          return (context) => {
+            return template.render(context)
+          }
+        },
+        prepare: (options, next) => {
+          options.compileOptions.environment = nunjucks.configure([
+            path.join(options.relativeTo || process.cwd(), options.path),
+            'node_modules/govuk-frontend/'
+          ], {
+            autoescape: true,
+            watch: false
+          })
+
+          return next()
+        }
+      }
+    },
+    path: '../views',
+    relativeTo: __dirname,
+    isCached: !config.isDev,
+    context: {
+      appVersion: pkg.version,
+      assetPath: '/assets',
+      serviceName: 'Conduit',
+      pageTitle: 'Conduit - GOV.UK',
+      analyticsAccount
+    }
+  }
+}
