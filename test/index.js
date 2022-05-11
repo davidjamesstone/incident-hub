cf25b629b99f1d2728d1d4a90114a8cb2e2b198f
davidjamesstone <davidjamesstone@gmail.com>
2022-05-11 13:54:22 +0100

    test: Comment out example tests

--- test/index.js
+++ test/index.js
@@ -22,41 +22,41 @@ lab.experiment('Web test', () => {
     Code.expect(response.headers['content-type']).to.include('text/html')
   })
 
-  lab.test('POST / route works', async () => {
-    const options = {
-      method: 'POST',
-      url: '/',
-      payload: {
-        email: 'a@b.com'
-      }
-    }
+  // lab.test('POST / route works', async () => {
+  //   const options = {
+  //     method: 'POST',
+  //     url: '/',
+  //     payload: {
+  //       email: 'a@b.com'
+  //     }
+  //   }
 
-    const response = await server.inject(options)
-    Code.expect(response.statusCode).to.equal(200)
-    Code.expect(response.headers['content-type']).to.include('text/html')
-  })
+  //   const response = await server.inject(options)
+  //   Code.expect(response.statusCode).to.equal(200)
+  //   Code.expect(response.headers['content-type']).to.include('text/html')
+  // })
 
-  lab.test('POST / route fails with invalid payload', async () => {
-    const options = {
-      method: 'POST',
-      url: '/',
-      payload: {
-        email: 'a@b'
-      }
-    }
+  // lab.test('POST / route fails with invalid payload', async () => {
+  //   const options = {
+  //     method: 'POST',
+  //     url: '/',
+  //     payload: {
+  //       email: 'a@b'
+  //     }
+  //   }
 
-    const response = await server.inject(options)
-    Code.expect(response.statusCode).to.equal(400)
-  })
+  //   const response = await server.inject(options)
+  //   Code.expect(response.statusCode).to.equal(400)
+  // })
 
-  lab.test('GET /about route works', async () => {
-    const options = {
-      method: 'GET',
-      url: '/about'
-    }
+  // lab.test('GET /about route works', async () => {
+  //   const options = {
+  //     method: 'GET',
+  //     url: '/about'
+  //   }
 
-    const response = await server.inject(options)
-    Code.expect(response.statusCode).to.equal(200)
-    Code.expect(response.headers['content-type']).to.include('text/html')
-  })
+  //   const response = await server.inject(options)
+  //   Code.expect(response.statusCode).to.equal(200)
+  //   Code.expect(response.headers['content-type']).to.include('text/html')
+  // })
 })
